ae8067374eb989bfee886bccdfd496f443b7c714
Ankit Prajapati <ap8424856@gmail.com>
2025-11-15 18:06:58 +0530

    fixed xdg issue

--- common/desktop.nix
+++ common/desktop.nix
@@ -61,6 +61,6 @@
   # XDG portal integration
   xdg.portal = {
     enable = true;
-    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+    configPackages = [ pkgs.xdg-desktop-portal-gtk ];
   };
 }
