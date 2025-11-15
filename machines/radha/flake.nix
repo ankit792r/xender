3e66fdfabf92ea27ccaca12f9744eedb1f1de689
Ankit Prajapati <ap8424856@gmail.com>
2025-11-15 15:16:53 +0530

    removed unstable packages

--- machines/radha/flake.nix
+++ machines/radha/flake.nix
@@ -3,7 +3,6 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     home-manager = {
         url = "github:nix-community/home-manager";
         inputs.nixpkgs.follows = "nixpkgs";
