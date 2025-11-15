692e0a0ba841afe991d608c2076a3d26528a6bfc
Ankit Prajapati <ap8424856@gmail.com>
2025-11-15 17:43:25 +0530

    fixed sddm

--- common/sddm.nix
+++ common/sddm.nix
@@ -5,34 +5,10 @@
   services.displayManager.sddm = {
     enable = true;
     wayland.enable = true;
-    theme = "maldives";
-
-    settings = {
-      Theme = {
-        CursorTheme = "Adwaita";
-        Font = "Noto Sans, 10";
-      };
-      General = {
-        Numlock = "on";
-      };
-    };
   };
 
   # X11 backend (for apps that need it)
-  services.xserver = {
-    enable = true;
-    xkb = {
-      layout = "us";
-      options = "caps:escape";
-    };
-    libinput = {
-      enable = true;
-      touchpad = {
-        naturalScrolling = true;
-        tapping = true;
-      };
-    };
-  };
+  services.xserver.enable = true;
 
   # Fonts (NixOS 25.05+ compatible)
   fonts = {
