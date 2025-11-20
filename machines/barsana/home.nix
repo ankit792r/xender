13b70350bc8b4abe869921c9905ac8e658feb98c
Ankit Prajapati <77499177+ankit792r@users.noreply.github.com>
2025-11-20 10:22:49 +0000

    removed extra apps from home

--- machines/barsana/home.nix
+++ machines/barsana/home.nix
@@ -2,10 +2,7 @@
     home.stateVersion = "25.05";
     programs.firefox.enable = true;
     home.packages = with pkgs; [
-        tmux
-        ranger
         mpv
         vlc
-        zathura
     ];
 }
