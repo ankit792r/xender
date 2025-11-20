a45845263a5bdee6316a42bce7d90b45a69c803e
Ankit Prajapati <77499177+ankit792r@users.noreply.github.com>
2025-11-20 09:34:33 +0000

    added barsana

--- flake.nix
+++ flake.nix
@@ -3,11 +3,13 @@
     inputs = {
         vrindavan.url = "path:./machines/vrindavan";
         gokul.url = "path:./machines/gokul";
+        barsana.url = "path:./machines/barsana";
     };
-    outputs = { self, vrindavan, gokul, ... }: {
+    outputs = { self, vrindavan, gokul, barsana, ... }: {
         nixosConfigurations = {
             gokul = gokul.nixosConfigurations.gokul;
             vrindavan = vrindavan.nixosConfigurations.vrindavan;
+            barsana = barsana.nixosConfigurations.barsana;
         };
-    };
+    };  
 }
---
+++ machines/barsana/config.nix
@@ -0,0 +1,23 @@
+{ config, pkgs, ... }: {
+    imports = [
+        ./hardware-configuration.nix
+        ../../common/boot.nix
+        ../../common/network.nix
+        ../../common/nix-conf.nix
+        ../../common/users.nix
+        ../../common/gnome.nix
+        ../../common/packages.nix
+        ../../common/services.nix
+        ../../common/desktop.nix
+    ];
+
+    networking.hostName = "Barsana";
+
+    home-manager = {
+        useGlobalPkgs = true;
+        useUserPackages = true;
+        users.ankit = import ./home.nix;
+    };
+    
+    system.stateVersion = "25.05";
+}
