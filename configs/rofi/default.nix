ce89ec1221e3b86d4dcd680a97e6aede40322a43
Ankit Prajapati <77499177+ankit792r@users.noreply.github.com>
2025-11-20 09:15:51 +0000

    updated some config

--- configs/rofi/default.nix
+++ configs/rofi/default.nix
@@ -1,5 +1,3 @@
 { pkgs, ... }: {
   programs.rofi.enable = true;
-  home.file.".config/rofi/config.rasi".source = ./config.rasi;
-  home.file.".config/rofi/theme.rasi".source = ./theme.rasi;
 }
--- configs/waybar/default.nix
+++ configs/waybar/default.nix
@@ -1,6 +1,3 @@
 { pkgs, ... }: {
   programs.waybar.enable = true;
-  
-  home.file.".config/waybar/config.jsonc".source = ./config.jsonc;
-  home.file.".config/waybar/style.css".source = ./style.css;
 }
