74e8ffa36b11a0c85286339ca5626c920a2e7570
Ankit Prajapati <77499177+ankit792r@users.noreply.github.com>
2025-11-20 06:24:37 +0000

    updated hw conf

--- gen-hw-config.sh
+++ gen-hw-config.sh
@@ -1,3 +1,1 @@
-sudo nixos-generate-config --show-hardware-config > machines/radha/hardware-configuration.nix
-sudo nixos-generate-config --show-hardware-config > machines/krishna/hardware-configuration.nix
-sudo nixos-generate-config --show-hardware-config > machines/temp/hardware-configuration.nix
+sudo nixos-generate-config --show-hardware-config > machines/*/hardware-configuration.nix
