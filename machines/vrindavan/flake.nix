43970a00c68edebf19112bc4c3c82280068c3642
Ankit Prajapati <ap8424856@gmail.com>
2025-11-29 16:58:54 +0530

    removed self

--- machines/vrindavan/flake.nix
+++ machines/vrindavan/flake.nix
@@ -13,7 +13,7 @@
     };
   };
 
-  outputs = { self, nixpkgs, home, stylix, ... }@inputs: {
+  outputs = { nixpkgs, home, stylix, ... }@inputs: {
       nixosConfigurations.vrindavan = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         specialArgs = { inherit inputs; };
