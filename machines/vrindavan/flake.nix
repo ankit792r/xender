22aa834d236bf8742a196ec8d75300026d98e974
Ankit Prajapati <ap8424856@gmail.com>
2025-11-29 17:53:14 +0530

    added stylix flake

--- machines/vrindavan/flake.nix
+++ machines/vrindavan/flake.nix
@@ -3,18 +3,20 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    stylix.url = "github:danth/stylix";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, stylix, ... }@inputs: {
     nixosConfigurations.vrindavan = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = { inherit inputs; };
       modules = [
         ./config.nix
+        stylix.nixosModules.stylix
         home-manager.nixosModules.home-manager
       ];
     };
