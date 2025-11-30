4f4b05cbe9caa1845d8f9329c5adea68a7b0cbb9
Ankit Prajapati <ap8424856@gmail.com>
2025-11-30 12:10:16 +0530

    fixed fire pro

--- machines/vrindavan/home.nix
+++ machines/vrindavan/home.nix
@@ -5,7 +5,13 @@
         ../../packages/office.nix
     ];
 
-    programs.firefox.enable = true;
+    programs.firefox = {
+        enable = true;
+        profiles = {
+            default = {};
+            work = { bookmarks = []; };
+        };
+    };
     home.username = var.username;
     home.homeDirectory = "/home/${var.username}";
     home.stateVersion = var.homeManagerVersion;
