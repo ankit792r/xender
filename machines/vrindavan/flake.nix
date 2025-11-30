8aa6723e74f481b0418765328cb09f714d7aeda5
Ankit Prajapati <ap8424856@gmail.com>
2025-11-30 11:58:40 +0530

    pined branch

--- machines/vrindavan/flake.nix
+++ machines/vrindavan/flake.nix
@@ -2,7 +2,7 @@
   description = "Vrindavan - The city of Radha and Krishna";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     home-manager = {
       url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -10,6 +10,7 @@
     stylix = {
       url = "github:nix-community/stylix/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
     };
   };
 
